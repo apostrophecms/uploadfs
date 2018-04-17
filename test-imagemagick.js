3dc8c3ba97f4df1cf63902d0157eae9bfe897858
pj <starsinmypockets@gmail.com>
2018-04-17 16:33:02 -0400

    Separate tests for imagemagick and imagecrunch

--- test-imagemagick.js
+++ test-imagemagick.js
@@ -1,10 +1,10 @@
 var uploadfs = require('./uploadfs.js')();
 var fs = require('fs');
-var _ = require('lodash');
+var async = require('async');
 
 // Test the imagecrunch image backend, written specifically for Macs
 
-var localOptions = { storage: 'local', image: 'imagecrunch', uploadsPath: __dirname + '/test', uploadsUrl: 'http://localhost:3000/test' };
+var localOptions = { storage: 'local', image: 'imagemagick', uploadsPath: __dirname + '/test', uploadsUrl: 'http://localhost:3000/test' };
 
 var imageSizes = [
   {
@@ -25,13 +25,25 @@ var imageSizes = [
 ];
 
 var tempPath = __dirname + '/temp';
+var basePath = '/images/profiles/me';
+var testFileSizes = {};
 
 localOptions.imageSizes = imageSizes;
 localOptions.tempPath = tempPath;
+localOptions.backend = 'local';
 
-localTestStart();
+localTestStart(function () {
+  console.log("RERUN TESTS WITH IMAGEMIN OPTION ENABLED");
+  localOptions.imagemin = true;
+  localTestStart(function () {
+    console.log("Tests, done");
+    process.exit(0);
+  });
+});
 
-function localTestStart() {
+// run again with imagemin enabled
+
+function localTestStart(cb) {
   var options = localOptions;
   console.log('Initializing uploadfs for the ' + options.backend + ' storage backend with the imagecrunch image backend');
   uploadfs.init(options, function(e) {
@@ -40,6 +52,7 @@ function localTestStart() {
       console.log(e);
       process.exit(1);
     }
+    console.log('uploadfs.init', this.options);
     testCopyImageIn();
   });
 
@@ -47,7 +60,7 @@ function localTestStart() {
     console.log('testing copyImageIn');
 
     // Note copyImageIn adds an extension for us
-    uploadfs.copyImageIn('test.jpg', '/images/profiles/me', function(e, info) {
+    uploadfs.copyImageIn('test.jpg', basePath, function(e, info) {
       if (e) {
         console.log('testCopyImageIn failed:');
         console.log(e);
@@ -74,28 +87,53 @@ function localTestStart() {
       }
 
       var stats = fs.statSync('test/images/profiles/me.jpg');
+
       if (!stats.size) {
         console.log('Copied image is empty or missing');
         process.exit(1);
       }
+
       // We already tested remove, just do it to mop up
       console.log('Removing files...');
-      uploadfs.remove('/images/profiles/me.jpg', function(e) { });
-      _.each(imageSizes, function(size) {
-        var name = info.basePath + '.' + size.name + '.jpg';
-        var stats = fs.statSync('test' + name);
-        if (!stats.size) {
-          console.log('Scaled and copied image is empty or missing (2)');
-          process.exit(1);
-        }
-        // We already tested remove, just do it to mop up
-        uploadfs.remove(info.basePath + '.' + size.name + '.jpg', function(e) { });
-      });
-      testCopyImageInCrop();
+      uploadfs.remove('/images/profiles/me.jpg', function(e) {
+        async.each(imageSizes, function(size, callback) {
+          var name = info.basePath + '.' + size.name + '.jpg';
+          var stats = fs.statSync('test' + name);
+          if (!stats.size) {
+            console.log('Scaled and copied image is empty or missing (2)');
+            process.exit(1);
+          }
+
+          // check minned versions again unminned
+          if (options.imagemin) {
+            testFileSizes[size.name + '_min'] = stats.size;
+          } else {
+            testFileSizes[size.name] = stats.size;
+          }
+
+          console.log("SIZES", testFileSizes);
+
+          if (testFileSizes[size.name + '_min'] >= testFileSizes[size.name]) {
+            console.log('Test fails, minned file should be smaller than unminned');
+            process.exit(1);
+          }
+
+          // We already tested remove, just do it to mop up
+          uploadfs.remove(info.basePath + '.' + size.name + '.jpg', function(e) {
+            callback();
+          });
+        }, function(err) {
+          if (err) {
+            console.log("Test failed", err);
+            process.exit(1);
+          }
+          testCopyImageInCrop(cb);
+        });
+      }); // remove me.jpg
     });
   }
 
-  function testCopyImageInCrop() {
+  function testCopyImageInCrop(cb) {
     console.log('testing copyImageIn with cropping');
 
     // Note copyImageIn adds an extension for us
@@ -121,29 +159,35 @@ function localTestStart() {
       }
 
       var stats = fs.statSync('test/images/profiles/me-cropped.jpg');
+
       if (!stats.size) {
         console.log('Copied image is empty or missing');
         process.exit(1);
       }
+
       // We already tested remove, just do it to mop up
       console.log('Removing files...');
-      uploadfs.remove('/images/profiles/me-cropped.jpg', function(e) { });
-      _.each(imageSizes, function(size) {
-        var name = info.basePath + '.' + size.name + '.jpg';
-        var stats = fs.statSync('test' + name);
-        if (!stats.size) {
-          console.log('Scaled and copied image is empty or missing (2)');
-          process.exit(1);
-        }
-        // We already tested remove, just do it to mop up
-        uploadfs.remove(info.basePath + '.' + size.name + '.jpg', function(e) { });
+      uploadfs.remove(`${basePath}-cropped.jpg`, function(e) {
+        async.each(imageSizes, function(size, callback) {
+          var name = info.basePath + '.' + size.name + '.jpg';
+          var stats = fs.statSync('test' + name);
+          if (!stats.size) {
+            console.log('Scaled and copied image is empty or missing (2)');
+            process.exit(1);
+          }
+          // We already tested remove, just do it to mop up
+          uploadfs.remove(info.basePath + '.' + size.name + '.jpg', function(e) {
+            callback(e);
+          });
+        }, function (err) {
+          if (err) {
+            console.log("Remove file fails", err);
+            process.exit(1);
+          }
+          console.log("Files removed");
+          cb();
+        });
       });
-      success();
     });
-  }
-
-  function success() {
-    console.log('All tests passing.');
-    process.exit(0);
   }
 }
