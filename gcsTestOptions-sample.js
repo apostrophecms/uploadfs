e03c267e38cf821b2f52b6c894a1b163b70caa1d
nickbauman <nick.bauman@cirrusmachina.com>
2019-01-21 22:44:28 -0600

    Adds backend for Google Cloud Storage using GCP service account and the official node client for GCS

---
+++ gcsTestOptions-sample.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // See https://cloud.google.com/docs/authentication/getting-started
+  // basically you want a service account file on the filesystem with
+  // the ENV variable GOOGLE_APPLICATION_CREDENTIALS pointing to it
+  // If you are getting `Error: Invalid Grant`, this is likely your problem
+  backend: 'gcs',
+  bucket: 'yourownbucketnamefromgcs',
+  region: 'us-west-2'
+};
