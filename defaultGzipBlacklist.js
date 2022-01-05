e8d7b2068e7d0b4823b76376d9e31c13470917a6
Christian Litzlbauer <christian.litzlbauer@alphaport.at>
2022-01-05 20:44:33 +0100

    remove pdf from gzip blacklist again

--- defaultGzipBlacklist.js
+++ defaultGzipBlacklist.js
@@ -1,4 +1,4 @@
 // By default, the following file types will not be gzipped.
 // Each is either (a) precompressed, or (b) poorly handled by a
 // browser with significant market share if compressed
-module.exports = [ 'jpg', 'png', 'zip', 'gzip', 'png', 'xls', 'docx', 'gif', 'mp4', 'pdf' ];
+module.exports = [ 'jpg', 'png', 'zip', 'gzip', 'png', 'xls', 'docx', 'gif', 'mp4' ];
